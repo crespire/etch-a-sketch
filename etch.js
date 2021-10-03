1dcc873307d0644c77ac3f8927ef1fcbe93ef0a7
crespire <li.simmon@gmail.com>
2021-10-03 16:40:31 -0400

    Algorithm completed, I think we're good to go.

--- etch.js
+++ etch.js
@@ -2,9 +2,8 @@
  * Set up variables in the global scope:
  *      * set up a reference to the parent container. We will add eventListeners to this, plus use it to add the grid of divs we will generate in Javascript
  * 
- * Generate a block of divs to use for the drawing area.
+ * Generate a grid of divs to use for the drawing area, make sure it has the right class so we can work with it later.
  * Append those divs to the maincontainer in HTML.
- * For each div inside the drawing area, assign "pixel" class so we know it's an element we can manipulate.
  * 
  * Add event listeners to the parent container. I think it is best to start with 3 event listeners so we can get the behaviour down. Once we have that, we can think about making the code more concise.
  *  EventListeners
@@ -17,9 +16,9 @@
  *      - For a 'mouseup' event, turn off "drawning" boolean
  */
 
-
+/**
 window.addEventListener('mousemove', function (e){
     if (e.target.classList[0] === 'pixel') { // Checks if the element is one we are looking to manipulate.
         console.dir(e);        
     }
-});
+}); */
