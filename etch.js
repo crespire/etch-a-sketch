096eb552060b3350e79f78be609231467ab187f6
crespire <li.simmon@gmail.com>
2021-10-03 22:07:49 -0400

    Added the grid element JavaScript, and added basic styling so the grid is always made to spec. To do still: handlers and flipping each pixel 'on', and adding the button to clear and ask for a new grid

--- etch.js
+++ etch.js
@@ -3,8 +3,13 @@
  *      * set up a reference to the parent container. We will add eventListeners to this, plus use it to add the grid of divs we will generate in Javascript
  * 
  * Generate a grid of divs to use for the drawing area, make sure it has the right class so we can work with it later.
+ *       let pixelContainer = getDiv with class maincontainer
+ *       create a function createDiv that returns a new div with the class we want.
+ *
+ *       let drawArea = new Array(16).fill( new Array(16).fill(createDiv);
+ *      
  * Append those divs to the maincontainer in HTML.
- * 
+ *       
  * Add event listeners to the parent container. I think it is best to start with 3 event listeners so we can get the behaviour down. Once we have that, we can think about making the code more concise.
  *  EventListeners
  *      - For a 'mousedown' event, turn on "drawing" boolean
@@ -22,3 +27,33 @@ window.addEventListener('mousemove', function (e){
         console.dir(e);        
     }
 }); */
+
+// Makes the correct size draw area and returns it as a documentFragment
+function makeDrawArea(x,y,strClass) {
+    // Helper function to return a new div
+    function makeCell(sClass) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add(sClass);
+        return newDiv;
+    }
+
+    let docFrag = document.createDocumentFragment();
+    let maxElements = x * y;
+
+    for (let i = 0; i < maxElements; i++) {
+        docFrag.appendChild(makeCell(strClass));
+    }
+
+    return docFrag;
+}
+
+// Set up document
+let mainContainer = document.querySelector('.mainContainer'); // Get the main container DOM element
+let gridLength = 16; // Set the length of one side, this is set as a variable as we will let the user change this later.
+let drawArea = makeDrawArea(gridLength, gridLength, 'pixel'); // Make the draw area cells all with specified class.
+
+ // Update the CSS varaible to match our draw area.
+document.documentElement.style.setProperty('--length', gridLength);
+
+// Add grid to our document.
+mainContainer.appendChild(drawArea);
